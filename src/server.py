2257b8e3b336b582ccdef04dae651f3539cebc13
Shawn Evans <shawnevans328@gmail.com>
2026-01-26 19:43:45 -0500

    prod server

--- src/server.py
+++ src/server.py
@@ -8,8 +8,9 @@ def home():
     return "marnie bot is fully operational! <3"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
-
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+    
 def keep_alive():
     t = Thread(target=run)
     t.start()
