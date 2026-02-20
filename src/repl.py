9b3d1ed03b3d7fd2653f25f4d25a441a931d4032
Shawn Evans <shawnevans328@gmail.com>
2026-02-19 23:48:36 -0500

    repl update

--- src/repl.py
+++ src/repl.py
@@ -5,12 +5,11 @@ x = fetch_data.FetchData()
 print("******************************************")
 print("Welcome to the Marnie Repl, used for testing !dt without deploying the bot to production.")
 
-print(">>> Enter your command. Type exit() to quit.")
+print(">>> Enter your query. Type exit() to quit.")
 query = input(">>> ").lower().strip()
 
 while query != "exit()":
     print(x.dt(query))
-    print(">>> Enter your command. Type exit() to quit.")
     query = input(">>> ").lower().strip()  
 
 print("Thanks for using the Marnie repl.")
