520871ba137d16782d35e85040dc1f40c84f8a2f
Shawn Evans <shawnevans328@gmail.com>
2026-03-24 02:05:11 -0400

    msg

--- src/constants/help.py
+++ src/constants/help.py
@@ -1,9 +1,9 @@
 '''Marnie's help command.'''
 
 command = "!dt {query} - Returns information on Pokemon, Pokemon abilities, Pokemon items, or Pokemon moves.\n" \
-       "!pick {args...} - Selects a random option given a list of options.\n" \
-       "!randmon - Returns a random Pokemon.\n" \
-       "!muted - lists all timed out server members and the amount of time they are timed out for.\n" \
-       "!sprite - Returns a Pokemon's sprite.\n" \
-       "!shiny - Returns a Pokemon's shiny sprite.\n" \
-       "!randsprite - Returns a random Pokemon's sprite."
+          "!pick {args...} - Selects a random option given a list of options.\n" \
+          "!randmon - Returns a random Pokemon.\n" \
+          "!muted - lists all timed out server members and the amount of time they are timed out for.\n" \
+          "!sprite - Returns a Pokemon's sprite.\n" \
+          "!shiny - Returns a Pokemon's shiny sprite.\n" \
+          "!randsprite - Returns a random Pokemon's sprite."
